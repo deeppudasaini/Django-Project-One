eef1fb41fd513203c6dff438f7c278eea6bbf251
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2021-05-24 23:52:39 +0545

    templates

--- Hello/Hello/urls.py
+++ Hello/Hello/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = "Deep Admin"
+admin.site.site_title = "Deep Admin Portal"
+admin.site.index_title = "Welcome to Deep Admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
